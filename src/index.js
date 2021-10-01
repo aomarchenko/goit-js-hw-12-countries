eb9b8f0352a8670895701a8fedc4b7c4999b82f4
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-10-01 19:23:50 +0300

    ищет страну

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.scss';
+const debounceFunction = _.debounce(onImput, 500);
+const input = document.querySelector('.search-input');
+input.addEventListener('input', debounceFunction);
+let searchQuery;
+
+function onImput(e) {
+  searchQuery = document.querySelector('.search-input').value;
+  console.log(searchQuery);
+  fetch(`https://restcountries.com/v2/name/${searchQuery}`).then(r => r.json().then(console.log));
+}

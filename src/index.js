b4d5c7264c21ce0b46493c625a29bb418d599536
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-10-01 19:52:15 +0300

    разнес импорт в отдельный файл

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './sass/main.scss';
-const debounceFunction = _.debounce(onImput, 500);
+import fetchCountries from './js/fetchCountries';
+const debounceFunction = _.debounce(fetchCountries, 500);
 const input = document.querySelector('.search-input');
 input.addEventListener('input', debounceFunction);
 let searchQuery;
 
-function onImput(e) {
-  searchQuery = document.querySelector('.search-input').value;
-  console.log(searchQuery);
-  fetch(`https://restcountries.com/v2/name/${searchQuery}`).then(r => r.json().then(console.log));
-}
+// function fetchCountries(searchQuery) {
+//   console.log(searchQuery.target.value);
+//   searchQuery = searchQuery.target.value;
+//   console.log(searchQuery);
+//   fetch(`https://restcountries.com/v2/name/${searchQuery}`).then(r => r.json().then(console.log));
+// }
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,5 @@
+export default function fetchCountries(searchQuery) {
+  searchQuery = searchQuery.target.value;
+
+  fetch(`https://restcountries.com/v2/name/${searchQuery}`).then(r => r.json().then(console.log));
+}

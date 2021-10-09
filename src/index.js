108eb79f1745da967d3110c164788fdd46806d07
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-10-09 13:14:56 +0300

    spinner

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
 
+import { Spinner } from 'spin.js';
+
 const _ = require('lodash');
 const Handlebars = require('handlebars');
 const debounceFunction = _.debounce(searchQuery, 500);
@@ -17,12 +19,41 @@ const input = document.querySelector('.search-input');
 const cardContainer = document.querySelector('.card-container');
 input.addEventListener('input', debounceFunction);
 
+// ======================================================Spinner================================================
+const opts = {
+  lines: 13, // The number of lines to draw
+  length: 38, // The length of each line
+  width: 17, // The line thickness
+  radius: 45, // The radius of the inner circle
+  scale: 1, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  speed: 1, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-shrink', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: 'gray', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  top: '80%', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  zIndex: 2000000000, // The z-index (defaults to 2e9)
+  className: 'spinner', // The CSS class to assign to the spinner
+  position: 'absolute', // Element positioning
+};
+const spinner = new Spinner(opts);
+const target = cardContainer;
+// =========================================================================================
+
 function searchQuery(e) {
+  spinner.spin(target);
+
   let query;
   console.log(e.target.value);
   query = e.target.value;
   fetchCountries(query).then(data => {
+    spinner.stop();
     let markup = countryCardTemplate(data);
+
     if (data.length === 1) cardContainer.innerHTML = markup;
     else if (data.length <= 10) {
       markup = countryListTemplate(data);
